4c9fb6e8ac6d6c3fca50541fd54f01312ec27aa7
kevincheng2 <chengyf112@gamil.com>
2024-03-15 22:33:31 +0800

    update code

--- scripts/Informer.sh
+++ scripts/Informer.sh
@@ -1,26 +1,26 @@
 export CUDA_VISIBLE_DEVICES=3
 
 
-python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --model_id trace_96_96 \
-  --model Informer \
-  --features SYSCALL \
-  --seq_len 96 \
-  --label_len 76 \
-  --pred_len 96 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 21 \
-  --dec_in 21 \
-  --c_out 393 \
-  --itr 1 \
-  --train_epochs 64 \
-  --gpu 0 \
-  --batch_size 32 \
-  --learning_rate 0.0001 \
+# python -u run.py \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --model_id trace_96_96 \
+#   --model Informer \
+#   --features SYSCALL \
+#   --seq_len 96 \
+#   --label_len 76 \
+#   --pred_len 96 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 21 \
+#   --dec_in 21 \
+#   --c_out 393 \
+#   --itr 1 \
+#   --train_epochs 64 \
+#   --gpu 0 \
+#   --batch_size 32 \
+#   --learning_rate 0.0001 \
 
 
 python -u run.py \

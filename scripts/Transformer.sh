25c96f491e75e195f689c57dc621b77d3b3f64d7
kevincheng2 <chengyf112@gamil.com>
2024-03-16 01:56:09 +0800

    update code

--- scripts/Transformer.sh
+++ scripts/Transformer.sh
@@ -1,26 +1,26 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2
 
 
-python -u run.py \
-  --is_training 1 \
-  --root_path ./dataset/ \
-  --model_id trace_96_96 \
-  --model Transformer \
-  --features SYSCALL \
-  --seq_len 96 \
-  --label_len 76 \
-  --pred_len 96 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 21 \
-  --dec_in 21 \
-  --c_out 393 \
-  --itr 1 \
-  --train_epochs 64 \
-  --gpu 0 \
-  --batch_size 32 \
-  --learning_rate 0.0001 \
+# python -u run.py \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --model_id trace_96_96 \
+#   --model Transformer \
+#   --features SYSCALL \
+#   --seq_len 96 \
+#   --label_len 76 \
+#   --pred_len 96 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 21 \
+#   --dec_in 21 \
+#   --c_out 393 \
+#   --itr 1 \
+#   --train_epochs 64 \
+#   --gpu 0 \
+#   --batch_size 32 \
+#   --learning_rate 0.0001 \
 
 
 python -u run.py \
@@ -41,7 +41,7 @@ python -u run.py \
   --itr 1 \
   --train_epochs 64 \
   --gpu 0 \
-  --batch_size 32 \
+  --batch_size 28 \
   --learning_rate 0.0001 \
 
 
@@ -63,7 +63,7 @@ python -u run.py \
   --itr 1 \
   --train_epochs 64 \
   --gpu 0 \
-  --batch_size 32 \
+  --batch_size 24 \
   --learning_rate 0.0001 \
 
 
@@ -85,5 +85,5 @@ python -u run.py \
   --itr 1 \
   --train_epochs 64 \
   --gpu 0 \
-  --batch_size 32 \
+  --batch_size 20 \
   --learning_rate 0.0001 \

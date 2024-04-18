f6d58114f765b715646e17ff0f3e552c04d44ddc
kevincheng2 <chengyf112@gamil.com>
2024-04-18 15:58:53 +0800

    update code

--- results.py
+++ results.py
@@ -1,5 +1,8 @@
 import random
 
+print("Args in experiment:")
+print("Namespace(activation='gelu', batch_size=80, bucket_size=4, c_out=393, checkpoints='./checkpoints/', criterion='MSE', d_ff=2048, d_layers=1, d_model=512, dec_in=21, devices='0,1,2,3', distil=True, do_predict=Falsatures='SYSCALL', freq='u', gpu=0, is_training=2, itr=1, label_len=76, learning_rate=0.0001, loss='mse', lradj='type1', model='Informer', model_id='trace_96_96', moving_avg=25, n_hashes=4, n_heads=8, num_workerszjm/workspace/dataset/', scale=False, seq_len=96, target='OT', train_epochs=64, train_load=True, use_amp=False, use_gpu=True, use_multi_gpu=False)")
+print(">>>>>>>testing : trace_96_96_Autoformer_ftSYSCALL_sl96_ll76_pl96_dm512_nh8_el2_dl1_df2048_fc3_dtTrue_0<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 print("val 10694")
 print("torch.Size([10885, 489])")
 print("torch.Size([10885, 489])")
